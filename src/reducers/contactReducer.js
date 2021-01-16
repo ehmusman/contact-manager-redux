7a263b76f5c61f72b62c11853cc29648c2119000
ehmusman <ehmusman@gmail.com>
2021-01-16 12:14:58 +0500

    redux boiler plate is ready

---
+++ src/reducers/contactReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    test: 'TT'
+};
+
+const contactReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export default contactReducer;
